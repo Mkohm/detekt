6c1f592398302b79e40d969c84e33ae7b1d6a39d
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-10 21:05:58 +0200

    Integrate with detekt-hint

--- buildSrc/src/main/kotlin/detekt.gradle.kts
+++ buildSrc/src/main/kotlin/detekt.gradle.kts
@@ -53,6 +53,7 @@ allprojects {
     dependencies {
         detekt(project(":detekt-cli"))
         detektPlugins(project(":detekt-formatting"))
+        detektPlugins("io.github.mkohm:detekt-hint:0.1.5")
     }
 }
 
@@ -92,6 +93,25 @@ val detektAll by tasks.registering(Detekt::class) {
         html.enabled = false
         txt.enabled = false
     }
+}
+
+val detektHint by tasks.registering(Detekt::class) {
+    description = "Running detekt-hint and outputs report for Danger to consume."
+    setSource(analysisDir)
+    disableDefaultRuleSets = true
+    config.setFrom(file("$rootDir/config/detekt/detekt-hint.yml"))
+    classpath.setFrom(detektClasspath)
+    reports {
+        xml {
+            enabled = true
+            destination = file("$buildDir/reports/detekt-hint-report.xml")
+        }
+        html.enabled = false
+        txt.enabled = false
+    }
+    include(kotlinFiles)
+    exclude(resourceFiles)
+    exclude(buildFiles)
 }
 
 val detektProjectBaseline by tasks.registering(DetektCreateBaselineTask::class) {
